85fb305d3c137e736633b5e8c86ebe0cfaf08db9
kaue matos <kaue178hotimail@gmail.com>
2023-12-06 22:00:23 -0300

    fix:mudando cor de fundo

--- src/assets/styles/default.ts
+++ src/assets/styles/default.ts
@@ -3,7 +3,7 @@ export const defaultTheme = {
     'main-bg': '#2a3447',
     'soft-bg': '#384256',
     'dark-bg': '#222b3c',
-    'black-900': '#000',
+    'black-900': '#111418',
     
     //text
     'main-color': 'white',
